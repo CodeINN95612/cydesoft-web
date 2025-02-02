786a626de21b03a11de9e9c1b4272ebb6b51108a
CodeINN95612 <insertnickname1@gmail.com>
2025-02-02 08:17:15 -0500

    cidesoft to cydesoft

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { Logo } from "./Logo";
 export function Header() {
   return (
     <header className="flex items-center justify-between max-w-5xl mx-auto px-8 pt-12">
-      <Logo className="h-12" />
+      <Logo className="h-10 sm:h-12" />
       <div className="flex items-center space-x-2 sm:space-x-4">
         <LanguageToggle />
         <DarkModeToggle />
